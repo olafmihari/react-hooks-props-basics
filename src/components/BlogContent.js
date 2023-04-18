6475793ac3957320003e1166410afa7a6fc86bfe
Olaf Mihari <olafnjenga@gmail.com>
2023-04-18 19:51:59 +0300

    completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -2,6 +2,18 @@ import React from "react";
 
 function BlogContent(props) {
   return <div id="blog-content">{props.articleText}</div>;
+  console.log(props)
+  if (!props.isPublished) {
+     //hide unpublished content
+    //return null
+    return null;
+  } else {
+     //show published 
+    return (<div>
+              <h1>{props.articleText}</h1>
+              <p>{props.minutesRead} minutes to read</p>
+    </div>)
+  }
 }
 
 export default BlogContent;
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,16 @@
 import React from "react";
+import BlogContent from "./BlogContent";
+import Comment from "./Comment";
 
-function Comment(props) {
-  return <div className="comment">{props.commentText}</div>;
+function BlogPost() {
+  return (
+    <div id="blog-post">
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
 }
 
-export default Comment;
+export default BlogPost;
